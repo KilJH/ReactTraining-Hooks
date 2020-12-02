866ccdbe0ee53873e34f13b3f6822c03b89f654a
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-02 16:23:25 +0900

    Chapter8 Hooks - useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,33 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+	const [name, setName] = useState('');
+	const [nickname, setNickname] = useState('');
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
+	useEffect(() => {
+		//렌더링 될 때마다 호출  componentDidMount + componentDidUpdate
+		console.log('렌더링이 완료되었습니다.');
+		console.log({
+			name,
+			nickname,
+		});
 
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+		//뒷정리(cleanup) 함수 호출
+		return () => {
+			console.log('cleanup');
+		};
 
-  return (
-    <div>
-      <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
-      </div>
-      <div>
-        <div>
-          <b>이름:</b> {name}
-        </div>
-        <div>
-          <b>닉네임:</b> {nickname}
-        </div>
-      </div>
-    </div>
-  );
+		//	}, []); // 배열 추가 시 마운트,언마운트(cleanup) 될 때만 실행
+	}, [name]); // name 값이 업데이트 될 때만 실행
+
+	const onChangeName = (e) => {
+		setName(e.target.value);
+	};
+
+	const onChangeNickname = (e) => {
+		setNickname(e.target.value);
+	};
+
+	return (
+		<div>
+			<div>
+				<input value={name} onChange={onChangeName} />
+				<input value={nickname} onChange={onChangeNickname} />
+			</div>
+			<div>
+				<div>
+					<b>이름:</b> {name}
+				</div>
+				<div>
+					<b>닉네임:</b> {nickname}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Info;
