a5fcec1fb5fbdafbc0126cabf2f17e0c57e580b2
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-02 16:48:49 +0900

    Chapter8 Hooks - useReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import logo from './logo.svg';
 import Info from './Info';
-import { useState } from 'react';
+import Counter from './Counter';
 
 const App = () => {
-	const [visible, setVisible] = useState(false);
-	return (
-		<div>
-			<button
-				onClick={() => {
-					setVisible(!visible);
-				}}
-			>
-				{visible ? '숨기기' : '보이기'}
-			</button>
-			<hr />
-			{visible && <Info />}
-		</div>
-	);
+	return <Info />;
 };
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,10 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+	return {
+		...state,
+		[action.name]: action.value,
+	};
+};
 
 const Info = () => {
-	const [name, setName] = useState('');
-	const [nickname, setNickname] = useState('');
+	//const [name, setName] = useState('');
+	//const [nickname, setNickname] = useState('');
 
-	useEffect(() => {
+	const [state, dispatch] = useReducer(reducer, {
+		name: '',
+		nickname: '',
+	});
+	const { name, nickname } = state;
+
+	const onChange = (e) => {
+		dispatch(e.target);
+	};
+
+	/* useState & useEffect
+    useEffect(() => {
 		//렌더링 될 때마다 호출  componentDidMount + componentDidUpdate
 		console.log('렌더링이 완료되었습니다.');
 		console.log({
@@ -27,12 +45,13 @@ const Info = () => {
 	const onChangeNickname = (e) => {
 		setNickname(e.target.value);
 	};
+*/
 
 	return (
 		<div>
 			<div>
-				<input value={name} onChange={onChangeName} />
-				<input value={nickname} onChange={onChangeNickname} />
+				<input name="name" value={name} onChange={onChange} />
+				<input name="nickname" value={nickname} onChange={onChange} />
 			</div>
 			<div>
 				<div>
